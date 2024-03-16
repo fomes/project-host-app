7d784d5d203788447a3f2196187c4356b11ed5bd
fomes <fgomesdeluna@gmail.com>
2024-03-16 09:26:02 -0300

    feat: assets images added

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex w-full h-screen">
+      <div>
+        <Image
+          alt="cover"
+          fill
+          src={"/assets/AuthPreview.png"}
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
